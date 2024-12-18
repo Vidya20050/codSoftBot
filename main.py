6b5f5b78e5799edefdb726af48376e0014b1de92
vidya G r <38433208-vidyargowda2005@users.noreply.replit.com>
2024-12-18 05:16:46 +0000

    updated chatbot response logic "fixed an issue with user input handling
    improved user experience by validating inputs more efficiently and more quickly

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-
 def chatbot():
     print("CodSoftBot: Hi! I am CodSoftBot. Type 'exit' to end the chat.")
+while True:
+  user_input = input("You: ")
+  if user_input == "exit":
+    print("CodSoftBot: bye! Have a nice day!")
+    break
+  else:
+    print("CodSoftBot: Hi! I am CodSoftBot. Type 'exit' to end the chat.")
 
-    while True:
-        user_input = input("You: ").lower()  
-
-        
-        if user_input in ["hi", "hello"]:
-            print("CodSoftBot: Hello! How can I help you?")
-        elif user_input == "what is your name?":
-            print("CodSoftBot: I am CodSoftBot, your friendly assistant.")
-        elif user_input == "exit":
-            print("CodSoftBot: bye! Have a nice day!")
-            break
-        else:
-            print("CodSoftBot: I'm sorry, I don't understand that.")
 
 chatbot()
